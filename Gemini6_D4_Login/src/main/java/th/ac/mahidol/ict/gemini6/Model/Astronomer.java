70591b0eea47d14359d77a7d01547af5e1cced93
pjpjpj2024 <161065660+pjpjpj2024@users.noreply.github.com>
2025-04-26 14:03:13 +0700

    Delete Gemini6_D4_Login directory

--- Gemini6_D4_Login/src/main/java/th/ac/mahidol/ict/gemini6/Model/Astronomer.java
+++
@@ -1,5 +0,0 @@
-package th.ac.mahidol.ict.gemini6.Model;
-
-public class Astronomer extends User{
-    private String astronomerID;
-}
--- Gemini6_D4_Login/src/main/java/th/ac/mahidol/ict/gemini6/Model/DataProcessing.java
+++
@@ -1,9 +0,0 @@
-package th.ac.mahidol.ict.gemini6.Model;
-
-import edu.gemini.app.ocs.model.DataProcRequirement;
-
-public class DataProcessing extends DataProcRequirement {
-    public DataProcessing() {
-
-    }
-}
